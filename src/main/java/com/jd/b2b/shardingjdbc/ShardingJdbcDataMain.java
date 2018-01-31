bd0b74d3af9b36bfe6dcf2254b7484323e66c405
wb-songwanke <songwanke@qq.com>
2018-01-31 15:49:54 +0800

    gitignore

--- src/main/java/com/jd/b2b/shardingjdbc/ShardingJdbcDataMain.java
+++ src/main/java/com/jd/b2b/shardingjdbc/ShardingJdbcDataMain.java
@@ -3,6 +3,9 @@ package com.jd.b2b.shardingjdbc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author songwanke
@@ -10,7 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan(basePackages = "com.jd.b2b.shardingjdbc.mapper")
 @SpringBootApplication
-public class ShardingJdbcDataMain{
+@EnableDiscoveryClient
+public class ShardingJdbcDataMain extends SpringBootServletInitializer{
 
     /**
      * 启动方法
@@ -20,4 +24,14 @@ public class ShardingJdbcDataMain{
         SpringApplication.run(ShardingJdbcDataMain.class,args);
     }
 
+    /**
+     * 覆盖configure()，把启动类Application注册进去。
+     * 外部web应用服务器构建Web Application Context的时候，会把启动类添加进去。
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ShardingJdbcDataMain.class);
+    }
 }
